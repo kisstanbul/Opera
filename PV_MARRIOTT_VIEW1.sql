4c1517e68a3de1d84f7fd7ad608eced3a3a11e42
Kaan Çelik <kcelik.istanbul@gmail.com>
2024-07-25 11:30:29 +0300

    Update PV_MARRIOTT_VIEW1.sql

--- PV_MARRIOTT_VIEW1.sql
+++ PV_MARRIOTT_VIEW1.sql
@@ -1,20 +1,22 @@
-CREATE PV_MARRIOTT_VIEW1 as
-SELECT resv_name_id RezNo
-     , Type_ "Type"
-     , resort HotelNo
-     , resort_name Hotel
-     , '-' AcentaGrup
-     , market_code Pazar
-     , travel_agent Acenta
-     , guest_country UlkeKodu
-     , RoomType OdaTipi
-     , arrival_date TarihGiris
-     , departure_date - arrival_date Geceleme
-     , no_rooms Room
-     , adults Yetiskin
-     , PaidChild CocukUcretli
-     , FreeChild CocukUcretsiz
-     , Baby Bebek
-     , ROUND(room_revenue * eur_exchange_rate,2) OdaGeliriEUR
-FROM pv_marriott_resv_dtl dtl 
-Where dtl.reservation_date = (SELECT business_date FROM businessdate b WHERE b.state = 'OPEN' AND b.resort = dtl.resort)
+CREATE VIEW PV_MARRIOTT_VIEW1 AS
+    SELECT resv_name_id RezNo
+         , Type_ "Type"
+         , resort HotelNo
+         , resort_name Hotel
+         , '-' AcentaGrup
+         , market_code Pazar
+         , travel_agent Acenta
+         , guest_country UlkeKodu
+         , RoomType OdaTipi
+         , arrival_date TarihGiris
+         , departure_date - arrival_date Geceleme
+         , no_rooms Room
+         , adults Yetiskin
+         , PaidChild CocukUcretli
+         , FreeChild CocukUcretsiz
+         , Baby Bebek
+         , ROUND ( room_revenue * eur_exchange_rate, 2) OdaGeliriEUR
+    FROM pv_marriott_resv_dtl dtl
+    WHERE dtl.reservation_date = (SELECT business_date
+                                  FROM businessdate b
+                                  WHERE b.state = 'OPEN' AND b.resort = dtl.resort)
